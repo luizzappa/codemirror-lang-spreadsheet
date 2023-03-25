5b9953e2dfd266093f5f96d3ad6588eb73b3484d
Luiz Zappa <luizzappa@hotmail.com>
2023-03-25 17:03:20 -0300

    test(): i18n RefErrorToken and ErrorToken

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-import {spreadsheetLanguage, setIdiom} from "../dist/index.js"
+import {spreadsheetLanguage, setLezerIdiom} from "../dist/index.js"
 import {fileTests} from "@lezer/generator/dist/test"
 
 import * as fs from "fs"
@@ -10,15 +10,15 @@ let caseDir = path.dirname(fileURLToPath(import.meta.url)),
 const isTxtFile = (file) => /\.txt$/.test(file),
   getFileName = (file) =>  /^[^\.]*/.exec(file)[0];
 
-// for (let file of fs.readdirSync(caseDir)) {
-//   if (!isTxtFile(file)) continue;
+for (let file of fs.readdirSync(caseDir)) {
+  if (!isTxtFile(file)) continue;
 
-//   let name = getFileName(file);
-//   describe(name, () => {
-//     for (let {name, run} of fileTests(fs.readFileSync(path.join(caseDir, file), "utf8"), file))
-//       it(name, () => run(spreadsheetLanguage.parser))
-//   })
-// }
+  let name = getFileName(file);
+  describe(name, () => {
+    for (let {name, run} of fileTests(fs.readFileSync(path.join(caseDir, file), "utf8"), file))
+      it(name, () => run(spreadsheetLanguage.parser))
+  })
+}
 
 for (let file of fs.readdirSync(i18nDir)) {
   if (!isTxtFile(file)) continue;
@@ -28,7 +28,7 @@ for (let file of fs.readdirSync(i18nDir)) {
   describe(name, () => {
     for (let {name, run} of fileTests(fs.readFileSync(path.join(i18nDir, file), "utf8"), file))
       it(name, () => {
-        setIdiom(idiom);
+        setLezerIdiom(idiom);
         return run(spreadsheetLanguage.parser);
       })
   })
