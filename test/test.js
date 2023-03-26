97374a0d8e29805b1a3abe5a86570dab2c2015a0
Luiz Zappa <luizzappa@hotmail.com>
2023-03-26 15:22:13 -0300

    test(): decimal separator

--- test/test.js
+++ test/test.js
@@ -1,11 +1,12 @@
-import {spreadsheetLanguage, changeIdiom} from "../dist/index.js"
+import {spreadsheetLanguage, changeIdiom, changeDecimalSeparator} from "../dist/index.js"
 import {fileTests} from "@lezer/generator/dist/test"
 
 import * as fs from "fs"
 import * as path from "path"
 import { fileURLToPath } from 'url';
 let caseDir = path.dirname(fileURLToPath(import.meta.url)),
-  i18nDir = path.join(caseDir, 'i18n');
+  i18nDir = path.join(caseDir, 'i18n'),
+  decimalSeparatorDir = path.join(caseDir, 'decimal-separator');
 
 const isTxtFile = (file) => /\.txt$/.test(file),
   getFileName = (file) =>  /^[^\.]*/.exec(file)[0];
@@ -13,7 +14,7 @@ const isTxtFile = (file) => /\.txt$/.test(file),
 for (let file of fs.readdirSync(caseDir)) {
   if (!isTxtFile(file)) continue;
 
-  let name = getFileName(file);
+  const name = getFileName(file);
   describe(name, () => {
     for (let {name, run} of fileTests(fs.readFileSync(path.join(caseDir, file), "utf8"), file))
       it(name, () => run(spreadsheetLanguage.parser))
@@ -33,4 +34,23 @@ for (let idiom of fs.readdirSync(i18nDir)) {
         })
     })
   }
+}
+
+for (let file of fs.readdirSync(decimalSeparatorDir)) {
+  if (!isTxtFile(file)) continue;
+
+  const decimalSeparatorName = getFileName(file),
+    caseName = `Decimal separator [${decimalSeparatorName}]`,
+    decimalSeparator = {
+      'comma': ',',
+      'dot': '.'
+    }[decimalSeparatorName];
+  describe(caseName, () => {
+    for (let {name, run} of fileTests(fs.readFileSync(path.join(decimalSeparatorDir, file), "utf8"), file)) {
+      it(name, () => {
+        changeDecimalSeparator(decimalSeparator);
+        run(spreadsheetLanguage.parser);
+    });
+    }
+  })
 }
